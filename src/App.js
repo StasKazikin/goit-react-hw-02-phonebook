f4c302bc783baac3435224b2c959916628fcdbad
Stanislav Kazikin <kazikinstas@gmail.com>
2021-03-08 12:44:21 +0200

    вынес контакты в файл initial_state.json

--- src/App.js
+++ src/App.js
@@ -2,15 +2,11 @@ import { Component } from "react";
 import ContactForm from "./components/ContactForm";
 import Filter from "./components/Filter";
 import ContactList from "./components/ContactList";
+import INITIAL_STATE from "./initial_state.json";
 
 class App extends Component {
   state = {
-    contacts: [
-      { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-      { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
-      { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-      { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
-    ],
+    contacts: [...INITIAL_STATE],
     filter: "",
   };
 
